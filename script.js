88de50d0fc8b3dd422920d4b416d075c04dfe0e2
Gaetan GIBERT <73164191+wyl312@users.noreply.github.com>
2021-01-12 14:44:33 +0100

    Exo 124 - Jquery Fini

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
-let player = $('.player__video').first();
-let progress = $('.progress__filled').first();
+
+//Recovery of items
+let play = $('.player__button').first();
+let prec = $('.player__button')[1];
+let next = $('.player__button')[2];
+let progress = $('#playbackRate').first();
+let active = false;
+let player = $('video');
+
+//Function play
+$(play).click(function(){
+    active ? active = false : active = true;
+
+    if(active){
+        $(player)[0].play();
+        $(play).text('||');
+    }else{
+        $(player)[0].pause();
+        $(play).text('►');
+    }
+});
+
+//Function progress bar
+$(player).each(function(){
+    $(player)[0].ontimeupdate = function(){
+        let duree = $(player)[0].duration;
+        let largeur_barre = $(progress).width();
+        let point_lecture = $(player)[0].currentTime;
+        let largeur_progress = point_lecture * largeur_barre / duree;
+        $(progress).val(largeur_progress);
+    }
+});
+
+
+//Function previous speed video
+$(prec).click(function(){
+    $(player)[0].currentTime -= 10;
+});
+
+//Function next speed video
+$(next).click(function(){
+    $(player)[0].currentTime += 10;
+});
+
